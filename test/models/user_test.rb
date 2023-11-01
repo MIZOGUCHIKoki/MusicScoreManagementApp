d3c7172619ef73e8369dd12be6fcded95d935767
MIZOGUCHI Koki <94270819+MIZOGUCHIKoki@users.noreply.github.com>
2023-11-01 17:49:40 +0900

    Add test of email validation with regular expression

--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -16,13 +16,17 @@ class UserTest < ActiveSupport::TestCase
     assert_not @user.valid?
   end
 
-  test 'email should be present' do
-    @user.email = ' '
-    assert_not @user.valid?
-  end
-
   test 'password should be present' do
     @user.password = ' '
     assert_not @user.valid?
+  end
+
+  test 'email validation should reject invalid addresses' do
+    invalid_addresses = %w[user@example,com user_at_foo.org user.name@example.
+                           foo@bar_baz.com foo@bar+baz.com]
+    invalid_addresses.each do |invalid_address|
+      @user.email = invalid_address
+      assert_not @user.valid?, "#{invalid_address.inspect} should be invalid"
+    end
   end
 end
