4ae40eb8bdb7c9bf1b356533aa07322253c6c117
MIZOGUCHI Koki <94270819+MIZOGUCHIKoki@users.noreply.github.com>
2023-11-02 18:16:01 +0900

    Apply `has_secure_password` with bcrypt

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,9 +2,14 @@
 
 class User < ApplicationRecord
   VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
-  validates :name,  presence: true, length: { maximum: 50 }
-  validates :email, presence: true,
-                    length: { maximum: 255 },
-                    format: { with: VALID_EMAIL_REGEX }
-  validates :password, presence: true
+  before_save { self.email = email.downcase } # email を小文字へ変換する．
+  validates :name,  presence: true, # 入力必須
+                    length: { maximum: 50 } # 最大長50
+  validates :email, presence: true, # 入力必須
+                    length: { maximum: 255 }, # 最大長255
+                    format: { with: VALID_EMAIL_REGEX }, # FORMATに沿っているか？
+                    uniqueness: { case_sensitive: false } # 一意なものであるか?
+  validates :password, presence: true, # 入力必須
+                       length: { in: 8..50 } # 8〜50文字
+  has_secure_password # NEED `bcrypt`
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,13 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2023_11_01_081203) do
+ActiveRecord::Schema[7.1].define(version: 2023_11_02_090805) do
   create_table "users", force: :cascade do |t|
     t.string "email"
     t.string "name"
-    t.string "password"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "password_digest"
+    t.index ["email"], name: "index_users_on_email", unique: true
   end
 
 end
