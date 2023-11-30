ef9326d570bccdc61a0570dd1a3ade02aeb20155
MIZOGUCHI Koki <94270819+MIZOGUCHIKoki@users.noreply.github.com>
2023-11-30 15:10:29 +0900

    Update comments

--- app/controllers/scores_controller.rb
+++ app/controllers/scores_controller.rb
@@ -7,13 +7,13 @@ class ScoresController < ApplicationController
   # 詳細の表示 :GET
   def show; end
 
-  # 新しいスコアを作成するページの表示 :GET
+  # 新しい楽譜データを作成するページの表示 :GET
   def new; end
 
   # 編集画面の表示 :GET
   def edit; end
 
-  # 新しいスコアを作成する :POST
+  # 新しい楽譜データを作成する :POST
   def create; end
 
   # 編集の反映 :PATCH/PUT
