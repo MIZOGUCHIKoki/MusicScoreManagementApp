f2a04b2e8d6c69a93344dd905ff0984f0b663503
MIZOGUCHI Koki <94270819+MIZOGUCHIKoki@users.noreply.github.com>
2023-12-04 20:17:15 +0900

    Update `Users`model

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,8 +1,17 @@
 # frozen_string_literal: true
 
 class User < ApplicationRecord
+  # コールバック
+  before_save { self.email = self.email.downcase } # 保存前にemailを小文字に変換する
+
+  VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-]+(\.[a-z\d\-]+)*\.[a-z]+\z/i
   validates :name,  presence: true, # Presence（存在性；空白でない）
                     length: { maximum: 50 } # 最大長50
   validates :email, presence: true, # Presence（存在性；空白でない）
-                    length: { maximum: 255 } # 最大長255
+                    length: { maximum: 255 }, # 最大長255
+                    format: { with: VALID_EMAIL_REGEX }, # emailの正規表現
+                    uniqueness: true # 一意性の検証
+  has_secure_password
+  validates :password, presence: true, # Presence（存在性；空白でない）
+                       length: { minimum: 6 } # 最小長6
 end
