32946972bc4f4aefa902b31b6f8587ba1f9e8a8d
MIZOGUCHI Koki <94270819+MIZOGUCHIKoki@users.noreply.github.com>
2023-12-04 23:09:45 +0900

    Update

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -5,20 +5,43 @@ class UsersController < ApplicationController
   def index; end
 
   # 個々のデータを表示：GET
-  def show; end
+  def show
+    @user = User.find(params[:id])
+  end
 
   # 新規作成画面を表示：GET
-  def new; end
+  def new
+    @user = User.new
+  end
 
   # 編集画面を表示：GET
   def edit; end
 
   # 作成を実行：POST
-  def create; end
+  def create
+    # @user = User.new(params[:user])
+    # 上記はセキュリティ的な問題があるので user_params を使って，特定のparamsのみを引き渡す
+    @user = User.new(user_params)
+    if @user.save
+      flash[:success] = '登録が完了しました'
+      redirect_to scores_path # スコア一覧へ
+    else
+      render 'new', status: :unprocessable_entity
+    end
+  end
 
   # 更新を実行：PATCH/PUT
   def update; end
 
   # 削除を実行：DELETE
   def destroy; end
+
+  private
+
+  def user_params
+    # 入力 :user に対して，以下のみを戻す．
+    # :name, :email, :password, :password_confirmation
+    # "user" => { "name" => "X", "email" => "x@x.com", "password" => "xxxxxx", "password_confirmation" => "xxxxxx" }
+    params.require(:user).permit(:name, :email, :password, :password_confirmation)
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -8,8 +8,11 @@ Rails.application.routes.draw do
   get 'up' => 'rails/health#show', as: :rails_health_check
 
   # Defines the root path route ("/")
-  # root "posts#index"
+
+  root 'scores#index'
 
   resource :users
   resource :scores
+
+  get 'signup' => 'users#new'
 end
