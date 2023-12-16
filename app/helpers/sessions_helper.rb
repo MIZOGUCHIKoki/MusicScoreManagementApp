8c55bcc05a2713b03d6420803c5528824f7651d4
tanakaryo341 <ryouyu60136013@gmail.com>
2023-12-16 18:04:51 +0900

    fix Rubocop's point of view

--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -7,32 +7,38 @@ module SessionsHelper
     session[:user_session_token] = user.session.token
   end
 
-  def remeber(user); end
-  
-  #現在のuserを取得
-  def current_user; 
+  def remeber(user)
+  end
+
+  # 現在のuserを取得
+  def current_user
     if (user_id = session[:user_id])
       user = User.find_by(id: user_id)
       user if user && sessin[:sessin_token] == user.sessin_token
-    elsif (user_id cokkies.encrypted[:user_id])
+    elsif user_id cokkies.encrypted[:user_id]
       user = User.find_by(id: user_id)
-      if (user && user.authenticated?(cookies[:remember_token]))
+      if user&.authenticated?(cookies[:remember_token])
         sign_in user
         user
       end
     end
   end
-  
-  def forget(user); end
+
+  def forget(user)
+  end
 
   # ユーザがログインしているか確認
   def sign_in?
     !current_user.nil?
   end
 
-  def sign_out; end
-  def current_user?(user); 
+  def sign_out 
+  end
+
+  def current_user?(user)
     user && user == current_user
   end
-  def store_location; end
+
+  def store_location
+  end
 end
