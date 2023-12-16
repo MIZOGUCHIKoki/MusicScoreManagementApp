d78a83dbb4344977a0b21f699d7e13e26eca7519
TANAKA ryo <114972589+tanakaryo341@users.noreply.github.com>
2023-12-17 00:08:40 +0900

    楽器入力値の制限

--- app/models/score.rb
+++ app/models/score.rb
@@ -12,5 +12,5 @@ class Score < ApplicationRecord
   validates :composer,  length: { maximum: 255 }
   validates :arranger,  length: { maximum: 255 }
   validates :grade,     length: { maxumum: 5 }
-  # validates :piccoro, inclusion: { in:[0,1] }
+  validates :piccoro, inclusion: { in: [0, 1] }
 end
