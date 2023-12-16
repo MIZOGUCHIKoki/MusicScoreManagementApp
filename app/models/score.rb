57c953b932b92fccf3f9caa8da4e41c31bc86e24
tanakaryo341 <ryouyu60136013@gmail.com>
2023-12-16 19:06:10 +0900

    create scope and validates

--- app/models/score.rb
+++ app/models/score.rb
@@ -2,4 +2,15 @@
 
 class Score < ApplicationRecord
   belongs_to :user
+
+  grade_sort_no ->    { order(created_at: :desc) }
+  grade_sort_desc ->  { order('CAST(grade AS float) DESC') }
+  grade_sort_asc ->   { order('CAST(grade AS float) ASC') }
+
+  validates :name,      presence: true,
+                        length: { maximum: 50 }
+  validates :composer,  length: { maximum: 255 }
+  validates :arranger,  length: { maximum: 255 }
+  validates :grade,     length: { maxumum: 5 }
+  # validates :piccoro, inclusion: { in:[0,1] }
 end
