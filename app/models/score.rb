beb7be66018e68e12fb88cd3cf87d67b7734992f
Nakamura Yuki <114973180+1250352@users.noreply.github.com>
2023-12-17 10:18:21 +0900

    score_searchスコープを実装

--- app/models/score.rb
+++ app/models/score.rb
@@ -15,4 +15,20 @@ class Score < ApplicationRecord
   validates :piccoro, inclusion: { in: [0, 1] }
   validates :o_flute, inclusion: { in: [0, 1] }
   validates :oboe,    inclusion: { in: [0, 1] }
+
+  scope :score_search, lambda { |search_params|
+    return if search_params.blank?
+
+    # search_paramsが存在しないもしくは中身がないときの判定
+
+    name_like(search_params[:name])
+      .composer_like(search_params[:composer])
+      .arranger_like(search_params[:arranger])
+      .grade_like(search_params[:grade])
+  }
+  scope :name_like, ->(name) { where('name LIKE ?', "%#{name}%") if name.present? }
+  scope :composer_like, ->(composer) { where('composer LIKE ?', "%#{composer}%") if composer.present? }
+  scope :arranger_like, ->(arranger) { where('arranger LIKE ?', "%#{arranger}%") if arranger.present? }
+  scope :grade_like, ->(grade) { where('grade LIKE ?', "#{grade}%") if grade.present? }
+  # if ~.present?:~の文字列が空か存在しない場合に処理をスキップする
 end
