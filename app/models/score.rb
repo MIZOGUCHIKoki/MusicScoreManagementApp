03efd2503e2ae46d148a2c8fc31cb926d0023370
TANAKA ryo <114972589+tanakaryo341@users.noreply.github.com>
2023-12-17 19:14:25 +0900

    各楽器のvalidates追加.

--- app/models/score.rb
+++ app/models/score.rb
@@ -7,14 +7,34 @@ class Score < ApplicationRecord
   scope :grade_sort_desc, ->  { order('CAST(grade AS float) DESC') }
   scope :grade_sort_asc, ->   { order('CAST(grade AS float) ASC') }
 
-  validates :name,      presence: true,
-                        length: { maximum: 50 }
-  validates :composer,  length: { maximum: 255 }
-  validates :arranger,  length: { maximum: 255 }
-  validates :grade,     length: { maximum: 5 }
-  validates :piccoro, inclusion: { in: [0, 1] }
-  validates :o_flute, inclusion: { in: [0, 1] }
-  validates :oboe,    inclusion: { in: [0, 1] }
+  validates :name,          presence: true,
+                            length: { maximum: 50 }
+  validates :composer,      length: { maximum: 255 }
+  validates :arranger,      length: { maximum: 255 }
+  validates :grade,         length: { maximum: 5 }
+  # validates :time,        inculsion: { in:  }
+  validates :piccolo,       inclusion: { in: [0, 1] }
+  validates :c_flute,       inclusion: { in: [0, 1] }
+  validates :oboe,          inclusion: { in: [0, 1] }
+  validates :english_horn,  inclusion: { in: [0, 1] }
+  validates :e_clarinet,    inclusion: { in: [0, 1] }
+  validates :b_clarinet,    inclusion: { in: [0, 1] }
+  validates :b_bass_clarinet,    inclusion: { in: [0, 1] }
+  validates :bassoon,    inclusion: { in: [0, 1] }
+  validates :e_alto_saxophone,    inclusion: { in: [0, 1] }
+  validates :b_tenor_saxophone,    inclusion: { in: [0, 1] }
+  validates :b_baritone_saxophone,    inclusion: { in: [0, 1] }
+  validates :b_trumpet,    inclusion: { in: [0, 1] }
+  validates :f_horn,    inclusion: { in: [0, 1] }
+  validates :trombone,    inclusion: { in: [0, 1] }
+  validates :baritone,    inclusion: { in: [0, 1] }
+  validates :tuba,    inclusion: { in: [0, 1] }
+  validates :string_bass,    inclusion: { in: [0, 1] }
+  validates :piano,    inclusion: { in: [0, 1] }
+  validates :harp,    inclusion: { in: [0, 1] }
+  validates :timpani,    inclusion: { in: [0, 1] }
+  validates :drums,    inclusion: { in: [0, 1] }
+  validates :percussion,    inclusion: { in: [0, 1] }
 
   scope :score_search, lambda { |search_params|
     return if search_params.blank?
