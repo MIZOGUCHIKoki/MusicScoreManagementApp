2a53901ac1ff374c564906441a96c300df887ab6
MIKAMI Shu <114971799+MikamiShu@users.noreply.github.com>
2023-12-18 17:15:37 +0900

    User.user_serach を修正

--- app/models/user.rb
+++ app/models/user.rb
@@ -62,12 +62,14 @@ class User < ApplicationRecord
   def update_sign_in_at
     update_attribute(:last_signin_date, Time.zone.now)
   end
+
+
   scope :user_search, lambda { |search_params|
     return if search_params.blank?
 
     name_like(search_params[:name])
       .email_like(search_params[:email])
   }
-  scope :name_like, ->(search_word) { where('name LIKE ?', "%#{search_word}") }
-  scope :email_like, ->(search_word) { where('email LIKE ?', "%#{search_word}") }
+  scope :name_like, -> (search_word) {where('name LIKE ?', "%#{search_word}%")}
+  scope :email_like, -> (search_word) {where('email LIKE ?', "%#{search_word}%")}
 end
