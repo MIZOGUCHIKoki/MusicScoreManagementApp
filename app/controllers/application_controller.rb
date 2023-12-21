2bc875cda942dfa1193bed8bb55a7bea70d7ddd9
OKUHIRA Shunri <114981884+OkuhiraShunri@users.noreply.github.com>
2023-12-21 15:05:45 +0900

    edit signed_in_user

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -9,8 +9,9 @@ class ApplicationController < ActionController::Base
     return true if signed_in?
 
     store_location # ユーザがどこからアクセスしてきたか保存
-    flash[:danger] = 'ログインしてください' # フラッシュメッセージをセット
-    redirect_to controller: :SessionsController, action: :new # ログインページへリダイレクト
+    # flash[:danger] = 'ログインしてください' # フラッシュメッセージをセット
+    # redirect_to controller: :SessionsController, action: :new # ログインページへリダイレクト
+    redirect_to signin_path
     false
   end
 end
